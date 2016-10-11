556185281fb7cb47f3b6c1893f2ffb1ff28aae80
JoshKoiro <josh.koiro@gmail.com>
2016-10-11 15:32:37 -0400

    Created generate.js to generate random text strings

---
+++ generate.js
@@ -0,0 +1,14 @@
+function generatePassword(n,symbols) {
+    var length = n,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    if(symbols !== undefined){
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+|<>,.?/:;"
+    }
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+//TEST CODE
+// console.log(generatePassword(8));
