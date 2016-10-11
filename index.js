8ff9501cea41555c98bd5981a330147f62acfcdd
JoshKoiro <josh.koiro@gmail.com>
2016-10-11 15:38:15 -0400

    Added standard Vorpal root file as index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const vorpal = require('vorpal')();
+
+vorpal
+  .command('foo', 'Outputs "bar".')
+  .action(function(args, callback) {
+    this.log('bar');
+    callback();
+  });
+
+vorpal
+  .delimiter('|PasswordGen|>>')
+  .show();
